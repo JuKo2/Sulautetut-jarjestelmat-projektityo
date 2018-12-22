76faa9f88f68dcee9123f2e5577f24fe9a258669
Juuso Koskinen <juuso.a.koskinen@student.tut.fi>
2018-12-22 12:44:09 +0000

    TemperatureSensor.ino lisätty

---
+++ SODAQ_ExpLoRer/SODAQ_ExpLoRer_TemperatureSensor.ino
@@ -0,0 +1,27 @@
+
+
+#define debugSerial SerialUSB
+
+void setup() {
+  //block till serial monitor is opened
+  while(!debugSerial);
+
+  //begin usb serial
+  debugSerial.begin(57600);
+
+  //define temperature pin as input
+  pinMode(TEMP_SENSOR, INPUT);
+}
+
+void loop() {
+  //10mV per C, 0C is 500mV
+  float mVolts = (float)analogRead(TEMP_SENSOR) * 3300.0 / 1023.0;
+  float temp = (mVolts - 500.0) / 10.0;
+
+  debugSerial.print(temp);
+  debugSerial.println(" C");
+
+  delay(1000);
+}
+
+
